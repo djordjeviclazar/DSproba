df6cc912b285093888ca6b782ed7cc0d686517a1
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-08-30 18:51:37 +0200

    Added interface for service

---
+++ jun 2020/zad4-Loto/WcfServiceLoto/WcfServiceLoto/ILotoService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceLoto
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILotoService" in both code and config file together.
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = ILotoCallback]
+    public interface ILotoService
+    {
+
+        [OperationContract]
+        bool PrijaviSe(Prijava novaPrijava);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    [Serializable]
+    public class Prijava
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public int[] brojevi { get; set; }
+
+        [DataMember]
+        public string Nadimak { get; set; }
+    }
+}
---
+++ jun 2020/zad4-Loto/WcfServiceLoto/WcfServiceLoto/LotoService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceLoto
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LotoService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LotoService.svc or LotoService.svc.cs at the Solution Explorer and start debugging.
+
+    public class LotoService : ILotoService
+    {
+        public bool PrijaviSe(Prijava novaPrijava)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
